37781aba8453a84a7ba8830077c4f69549f134f3
Tomoya AMACHI <tomoya.amachi@gmail.com>
2019-06-04 23:08:19 +0900

    add rpm command in ci

--- pkg/utils/fetch_test.go
+++ pkg/utils/fetch_test.go
@@ -1,39 +1,9 @@
 package utils
 
 import (
-	"net/http"
 	"strings"
 	"testing"
 )
-
-func TestFetchURL(t *testing.T) {
-	var tests = map[string]struct {
-		url     string
-		cookie  *http.Cookie
-		expect  string
-		wantErr error
-	}{
-		"Github": {
-			url: "https://github.com/goodwithtech/docker-guard/releases/latest",
-			cookie: &http.Cookie{
-				Name:  "user_session",
-				Value: "guard",
-			},
-			expect: "hoge",
-		},
-	}
-
-	for testname, v := range tests {
-
-		result, err := fetchURL(v.url, v.cookie)
-		if err != nil {
-			t.Errorf("%s : fail to fetch %s", testname, v.url)
-		}
-		if string(result) != v.expect {
-			t.Errorf("%s : want %s, actual %s", testname, v.expect, string(result))
-		}
-	}
-}
 
 func TestFetchVersion(t *testing.T) {
 	result, err := FetchLatestVersion()
