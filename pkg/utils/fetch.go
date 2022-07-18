62a58509d9acbb88af453c6955598879a71477f3
masaya.suzuki <suzukimasaya428@gmail.com>
2022-07-18 11:49:43 +0900

    Avoid casting the result of fetchLocation

--- pkg/utils/fetch.go
+++ pkg/utils/fetch.go
@@ -52,5 +52,5 @@ func FetchLatestVersion(ctx context.Context) (version string, err error) {
 	if versionMatched := versionPattern.FindString(*body); versionMatched != "" {
 		return versionMatched, nil
 	}
-	return "", fmt.Errorf("not found version patterns parsing GH response: %s", string(body))
+	return "", fmt.Errorf("not found version patterns parsing GH response: %s", *body)
 }
