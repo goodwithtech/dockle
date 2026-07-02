53b7e8d1165c09d6956e645ad633f63d2c647543
Tomoya AMACHI <tomoya.amachi@gmail.com>
2026-07-02 10:57:32 +0900

    [機能] deckoder extractor/image/token を pkg/extractor へ移送

---
+++ pkg/extractor/image/token/gcr/gcr.go
@@ -0,0 +1,50 @@
+package gcr
+
+import (
+	"context"
+	"strings"
+
+	"github.com/goodwithtech/dockle/pkg/types"
+
+	"golang.org/x/xerrors"
+
+	"github.com/GoogleCloudPlatform/docker-credential-gcr/v2/config"
+	"github.com/GoogleCloudPlatform/docker-credential-gcr/v2/credhelper"
+	"github.com/GoogleCloudPlatform/docker-credential-gcr/v2/store"
+)
+
+type GCR struct {
+	Store  store.GCRCredStore
+	domain string
+}
+
+const gcrURL = "gcr.io"
+
+func (g *GCR) CheckOptions(domain string, d types.DockerOption) error {
+	if !strings.HasSuffix(domain, gcrURL) {
+		return xerrors.Errorf("GCR : %w", types.InvalidURLPattern)
+	}
+	g.domain = domain
+	if d.GcpCredPath != "" {
+		g.Store = store.NewGCRCredStore(d.GcpCredPath)
+	}
+	return nil
+}
+
+func (g *GCR) GetCredential(ctx context.Context) (username, password string, err error) {
+	var credStore store.GCRCredStore
+	if g.Store == nil {
+		credStore, err = store.DefaultGCRCredStore()
+		if err != nil {
+			return "", "", xerrors.Errorf("failed to get GCRCredStore: %w", err)
+		}
+	} else {
+		credStore = g.Store
+	}
+	userCfg, err := config.LoadUserConfig()
+	if err != nil {
+		return "", "", xerrors.Errorf("failed to load user config: %w", err)
+	}
+	helper := credhelper.NewGCRCredentialHelper(credStore, userCfg)
+	return helper.Get(g.domain)
+}
